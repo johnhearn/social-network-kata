9ff5dad3b6e3930ad872d6a3bc15ddf987930451
John <hearn.john@gmail.com>
2018-04-26 17:50:18 +0200

    Allow tests to be ignored with @ignored tag

--- src/test/cucumber/com/codurance/kata/socialnetwork/acceptance/RunCucumberTests.java
+++ src/test/cucumber/com/codurance/kata/socialnetwork/acceptance/RunCucumberTests.java
@@ -1,8 +1,10 @@
 package com.codurance.kata.socialnetwork.acceptance;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(tags = "not @ignored")
 public class RunCucumberTests {
 }
